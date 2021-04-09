38f481107dd012e5976f251b8bb2626e2c1069a5
芊里 <1227971437@qq.com>
2021-04-09 17:39:26 +0800

    头像上传

--- pages/ucenter/ucenter.vue
+++ pages/ucenter/ucenter.vue
@@ -114,6 +114,11 @@
 				checkUpdate();
 			},
 			goLogin() {
+				uni.navigateTo({
+					url: './edit/edit'
+				})
+				
+				return;
 				if (!this.login) {
 					console.log('点击前往登录');
 					uni.navigateTo({
