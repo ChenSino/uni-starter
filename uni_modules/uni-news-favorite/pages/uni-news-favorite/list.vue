92ce79947b3761b29cba3c8f999e420ebba64e84
芊里 <1227971437@qq.com>
2021-04-21 22:30:52 +0800

    积分/阅读文章

---
+++ uni_modules/uni-news-favorite/pages/uni-news-favorite/list.vue
@@ -0,0 +1,68 @@
+<template>
+  <view class="container">
+    <unicloud-db ref="udb" v-slot:default="{data, pagination, loading, hasMore, error}" collection="opendb-news-favorite" field="article_id">
+      <view v-if="error">{{error.message}}</view>
+      <view v-else-if="data">
+        <uni-list>
+          <uni-list-item v-for="(item, index) in data" :key="index" showArrow :clickable="true" @click="handleItemClick(item)">
+            <view slot="body">
+              <text>{{item.article_title || item.article_id}}</text>
+			  <uni-dateformat :date="item.create_date" format="yyyy-MM-dd hh:mm"
+			  	:threshold="[0, 0]" />
+            </view>
+          </uni-list-item>
+        </uni-list>
+      </view>
+      <uni-load-more :status="loading?'loading':(hasMore ? 'more' : 'noMore')"></uni-load-more>
+    </unicloud-db>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        loadMore: {
+          contentdown: '',
+          contentrefresh: '',
+          contentnomore: ''
+        }
+      }
+    },
+    onPullDownRefresh() {
+      this.$refs.udb.loadData({
+        clear: true
+      }, () => {
+        uni.stopPullDownRefresh()
+      })
+    },
+    onReachBottom() {
+      this.$refs.udb.loadMore()
+    },
+    methods: {
+      handleItemClick(item) {
+		  console.log(item);
+        uni.navigateTo({
+          url: '/pages/list/detail?id='+item.article_id+'&title='+(item.article_title || '')
+        })
+      },
+      fabClick() {
+        // 打开新增页面
+        uni.navigateTo({
+          url: './add',
+          events: {
+            // 监听新增数据成功后, 刷新当前页面数据
+            refreshData: () => {
+              this.$refs.udb.loadData({
+                clear: true
+              })
+            }
+          }
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ uni_modules/uni-news-favorite/readme.md
@@ -0,0 +1,1 @@
+# opendb-news-favorite
