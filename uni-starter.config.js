5da62ee227615c3f7e41cdbc4f52418a1b4eb2d6
linju-json <linju@dcloud.io>
2021-05-10 18:01:58 +0800

    因应用市场最新要求，登陆功能去掉默认同意隐私政策。改为用户手动点击。账号登陆添加图形验证码

---
+++ uni-starter.config.js
@@ -0,0 +1,63 @@
+//这是应用的配置页面，App.vue挂载到getApp().globalData.config
+module.exports = {
+	"h5": {
+		"url": "https://static-76ce2c5e-31c7-4d81-8fcf-ed1541ecbc6e.bspapp.com", //	前端网页托管的域名
+		 // 在h5端全局悬浮引导用户下载app的功能 更多自定义要求在/common/openApp.js中修改
+		"openApp": {
+			//点击悬浮下载栏后打开的网页链接
+			"openUrl": 'https://sj.qq.com/myapp/detail.htm?apkName=com.tencent.android.qqdownloader&info=6646FD239A6EBA9E2DEE5DFC7E18D867',
+			//左侧显示的应用名称
+			"appname": 'uni-starter',
+			//应用的图标
+			"logo": './static/logo.png',
+		}
+	},
+	"mp": {
+		"weixin": {
+			//微信小程序原始id，微信小程序分享时
+			"id": "gh_33446d7f7a26"
+		}
+	},
+	"router": {
+		//配置需要路由拦截的页面地址，在打开这些页面之前会自动检查（不联网）uni_id_token的值是否存在/过期等
+		"needLogin": [
+			"/pages/ucenter/userinfo/userinfo",
+			"/uni_modules/uni-news-favorite/pages/uni-news-favorite/list",
+			"/pages/ucenter/userinfo/uploadCutImageToUnicloud"
+		],
+		"login": ["smsCode","uniVerify", "username", "weixin", "apple"],
+		/* 
+			根据数组的第0项，决定登陆方式的第一优先级。
+			未列举到的，或设备环境不支持的选项，将被隐藏。
+			如果你需要在不同平台有不同的配置，直接用条件编译即可
+		*/
+	},
+	//关于应用
+	"about": {
+		//应用名称
+		"appName": "uni-starter",
+		//应用logo
+		"logo": "/static/logo.png",
+		//公司名称
+		"company": "数字天堂（北京）网络技术有限公司",
+		//口号
+		"slogan": "为开发而生",
+		//政策协议
+		"agreements": [{
+				"title": "用户服务协议", //协议名称
+				"url": "https://ask.dcloud.net.cn/protocol.html" //对应的网络链接
+			},
+			{
+				"title": "隐私政策",
+				"url": "https://ask.dcloud.net.cn/protocol.html"
+			}
+		],
+		//应用的链接，用于分享到第三方平台和生成关于我们页的二维码
+		"download": "https://m3w.cn/uniapp"
+	},
+	//用于打开应用市场评分界面
+	"marketId":{
+		"ios":"id1417078253",
+		"android":"123456"
+	}
+}
---
+++ uni_modules/opendb-feedback/pages/opendb-feedback/list.vue
@@ -0,0 +1,54 @@
+<template>
+	<view class="container">
+		<unicloud-db ref="udb" v-slot:default="{data, pagination, loading, hasMore, error}" collection="opendb-feedback"
+			field="content, title" where="is_reply == false" @load="isLoading == false" @error="isLoading == false">
+			<view v-if="data && data.length">
+				<uni-collapse :accordion="true">
+					<uni-collapse-item v-for="(item, index) in data" :key="index" :title="item.title" :show-animation="true">
+						<text class="content">{{ item.content }}</text>
+					</uni-collapse-item>
+				</uni-collapse>
+			</view>
+		</unicloud-db>
+		<uni-fab ref="fab" horizontal="right" vertical="bottom" :pop-menu="false" @fabClick="fabClick" />
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				isLoading:true,
+			}
+		},
+		methods: {
+			refreshData() {
+				this.$refs.udb.loadData({
+					clear: true
+				}, (res) => {
+					console.log(res);
+				})
+			},
+			fabClick() {
+				// 打开新增页面
+				uni.navigateTo({
+					url: './add',
+					events: {
+						// 监听新增数据成功后, 刷新当前页面数据
+						refreshData: () => {
+							this.$refs.udb.loadData({
+								clear: true
+							})
+						}
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.content{
+		padding: 20rpx;
+	}
+</style>
