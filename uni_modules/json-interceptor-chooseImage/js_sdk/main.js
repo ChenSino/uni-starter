6cd08ba17448c999a4bd2b8efc9f19bf5f168afa
linju-json <linju@dcloud.io>
2021-08-05 19:57:39 +0800

    测试发布功能

--- uni_modules/json-interceptor-chooseImage/js_sdk/main.js
+++ uni_modules/json-interceptor-chooseImage/js_sdk/main.js
@@ -31,8 +31,8 @@ export default function(){
 				console.log('e.errMsg === 2  ios无法拍照权限 ');
 				// 注：e.errCode === 8 ios无从相册选择图片的权限 api已内置无需自己用拦截器实现
 				uni.showModal({
-					title: "无法访问相册",
-					content: "当前无系统相册访问权限，建议前往设置",
+					title: "无法访问摄像头",
+					content: "当前无摄像头访问权限，建议前往设置",
 					confirmText: "前往设置",
 					success(e) {
 						if (e.confirm) {
