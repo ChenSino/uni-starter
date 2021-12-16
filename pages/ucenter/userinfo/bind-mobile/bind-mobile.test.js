d126a8e9807bd6e1a724f6ac472cfbdb9432d6d5
anne-lxm <1076217653@qq.com>
2021-12-16 17:01:44 +0800

    fix:jest bindmobile

--- pages/ucenter/userinfo/bind-mobile/bind-mobile.test.js
+++ pages/ucenter/userinfo/bind-mobile/bind-mobile.test.js
@@ -50,7 +50,8 @@ describe('pages/ucenter/userinfo/bind-mobile/bind-mobile.vue', () => {
 					await program.redirectTo('/pages/ucenter/login-page/pwd-login/pwd-login')
 					break;
 				case  60101:
-					expect(submitRes.msg).toBe("此手机号已绑定" || "手机号 is already bound")
+					// expect(submitRes.msg).toBe("此手机号已绑定" || "手机号 is already bound")
+					expect(submitRes.errCode).toBe("uni-id-account-already-bound")
 					break;
 				default:
 					break;
