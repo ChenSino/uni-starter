79f19dbb119759ab6abb2e668dd844d2b711dd77
linju <linju@dcloud.io>
2022-05-16 16:28:49 +0800

    解决多个db_init的冲突

--- pages/ucenter/login-page/register/register.vue
+++ pages/ucenter/login-page/register/register.vue
@@ -34,7 +34,7 @@ import mixin from '../common/login-page.mixin.js';
 					"nickname": "",
 					"password":"",
 					"pwd2":"",
-					"captcha":""
+					"captcha":false
 				},
 				rules,
 				agree:false,
