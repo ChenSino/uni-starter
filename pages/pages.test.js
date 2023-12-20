ab93fe5284b5969245895392df27650281831573
Anne_LXM <54163582+anne-lxm@users.noreply.github.com>
2023-12-20 20:05:09 +0800

    update test

---
+++ pages/pages.test.js
@@ -0,0 +1,28 @@
+let pageIndex = 0
+const pages = [
+	'/pages/ucenter/settings/settings',
+	'/pages/ucenter/ucenter',
+	'/uni_modules/uni-id-pages/pages/login/login-withoutpwd'
+]
+
+let page;
+describe('page screenshot test', () => {
+	beforeAll(async () => {
+		console.log("page screenshot test start");
+	});
+	beforeEach(async () => {
+		page = await program.reLaunch(pages[pageIndex]);
+		await page.waitFor(1000);
+	});
+	afterEach(() => {
+		pageIndex++;
+	});
+	afterAll(() => {
+		console.log("page screenshot test finish");
+	});
+	test.each(pages)('%s', async () => {
+		const image = await program.screenshot();
+		expect(image).toMatchImageSnapshot();
+		await page.waitFor(500);
+	})
+})
