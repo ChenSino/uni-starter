6a600fc9b5938ed97d11f301d0656ffc3ec5ec81
Anne_LXM <54163582+anne-lxm@users.noreply.github.com>
2023-12-22 17:03:18 +0800

    Update list.test.js

--- pages/list/list.test.js
+++ pages/list/list.test.js
@@ -6,9 +6,10 @@ describe('pages/list/list.vue', () => {
 	})
 	it('检测标题', async () => {
 		expect.assertions(1);
+		await page.waitFor(1000)
 		const getTitle = await page.data('dataList')
 		console.log('getTitle: ',getTitle);
-		expect((await page.data('dataList')).title).toBe('阿里小程序IDE官方内嵌uni-app，为开发者提供多端开发服务')
+		expect(getTitle.title).toBe('阿里小程序IDE官方内嵌uni-app，为开发者提供多端开发服务')
 	})
 	it('点击搜索跳转', async () => {
 		await page.callMethod('searchClick')
